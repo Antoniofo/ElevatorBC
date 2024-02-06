2798588e708fc99507ba65da2059856aa2479cc2
Antoniofo <antonio57201@gmail.com>
2024-02-06 19:44:05 +0100

    initial commit

---
+++ Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Interfaces;
+
+namespace ElevatorBC
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = true;
+
+        public string Message { get; set; } = "<br><br><br><br><br><br><br><br><br><br><br><br><size=18>Le joureur <color=red>%NAME%</color> a appuyer sur le bouton</size>";
+    }
+}
---
+++ ElevatorBC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using Exiled.Events;
+using Exiled.Events.EventArgs.Player;
+using UnityEngine;
+
+namespace ElevatorBC
+{
+    public class ElevatorBC : Plugin<Config>
+    {
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            Exiled.Events.Handlers.Player.InteractingElevator += OnPlayerInteractElevator;
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            Exiled.Events.Handlers.Player.InteractingElevator -= OnPlayerInteractElevator;
+        }
+
+        public void OnPlayerInteractElevator(InteractingElevatorEventArgs ev)
+        {
+
+            foreach (Player ply in Player.List)
+            {
+                if (Vector3.Distance(ply.Position, ev.Elevator.gameObject.transform.position) < 10.0f)
+                {
+                    ply.ShowHint(Config.Message.Replace("%NAME%", ev.Player.DisplayNickname));
+                }
+            }
+
+        }
+    }
+    
+}
