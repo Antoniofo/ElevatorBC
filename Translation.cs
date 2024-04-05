b39917de0e805078f9ec6cfa4a7a3ea4890ef0d3
Antoniofo <antonio57201@gmail.com>
2024-04-05 16:59:16 +0200

    fix some issues

--- Translation.cs
+++ Translation.cs
@@ -9,10 +9,8 @@ namespace ElevatorBC
 {
     public class Translation : ITranslation
     {
-        public string ElevatorMessage { get; set; } = "<br><br><br><br><br><br><br><br><br><br><br><br><size=18>The player <color=red>%NAME%</co
-lor> has pressed the button</size>";
+        public string ElevatorMessage { get; set; } = "<br><br><br><br><br><br><br><br><br><br><br><br><size=18>The player <color=red>%NAME%</color> has pressed the button</size>";
 
-        public string Scp914Message { get; set; } = "<br><br><br><br><br><br><br><br><br><br><br><br><size=18>The player <color=red>%NAME%</colo
-r> started 914 in <color=red>%MODE%</color></size>";
+        public string Scp914Message { get; set; } = "<br><br><br><br><br><br><br><br><br><br><br><br><size=18>The player <color=red>%NAME%</color> started 914 in <color=red>%MODE%</color></size>";
     }
 }
