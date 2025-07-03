b508330ed34d9e9d5a20d412488da16012c7e697
Antoniofo <antonio57201@gmail.com>
2025-07-03 10:20:48 +0200

    Changed: Added Checks + Exiled Bump

--- ElevatorBC.cs
+++ ElevatorBC.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Exiled.API.Features;
-using Exiled.API.Interfaces;
-using Exiled.Events;
 using Exiled.Events.EventArgs.Player;
 using Exiled.Events.EventArgs.Scp914;
+using Interactables.Interobjects;
 using UnityEngine;
 
 namespace ElevatorBC
@@ -18,46 +13,59 @@ namespace ElevatorBC
 
         public override string Name => "ElevatorBC";
 
-        public override Version Version => new Version(1,1,1);
+        public override Version Version => new Version(1, 2, 0);
 
-        public override Version RequiredExiledVersion => new Version(8, 11, 0);
+        public override Version RequiredExiledVersion => new Version(9, 6, 1);
 
         public override string Prefix => "elevatorbc";
+
         public override void OnEnabled()
-        {            
+        {
             Exiled.Events.Handlers.Player.InteractingElevator += OnPlayerInteractElevator;
             Exiled.Events.Handlers.Scp914.Activating += On914Activating;
             base.OnEnabled();
         }
 
         public override void OnDisabled()
-        {            
+        {
             Exiled.Events.Handlers.Player.InteractingElevator -= OnPlayerInteractElevator;
             Exiled.Events.Handlers.Scp914.Activating -= On914Activating;
             base.OnDisabled();
         }
 
-        public void On914Activating(ActivatingEventArgs ev)
+        private void On914Activating(ActivatingEventArgs ev)
         {
+            if (!ev.IsAllowed) return;
+
             foreach (Player ply in Player.List)
             {
+                if (ply is null || ply.IsNPC || ply.IsHost || ply.UserId is null)
+                    continue;
                 if (Vector3.Distance(ply.Position, Exiled.API.Features.Scp914.Transform.position) < Config.Distance)
                 {
-                    ply.ShowHint(Translation.Scp914Message.Replace("%NAME%", ev.Player.DisplayNickname).Replace("%MODE%",Exiled.API.Features.Scp914.KnobStatus.ToString()), Config.HintDuration);
+                    ply.ShowHint(Translation.Scp914Message
+                        .Replace("%NAME%", ev.Player.DisplayNickname)
+                        .Replace("%MODE%", Exiled.API.Features.Scp914.KnobStatus.ToString()), Config.HintDuration);
                 }
             }
         }
-        public void OnPlayerInteractElevator(InteractingElevatorEventArgs ev)
-        {
 
+        private void OnPlayerInteractElevator(InteractingElevatorEventArgs ev)
+        {
+            if (!ev.IsAllowed || (ev.Lift.Status != ElevatorChamber.ElevatorSequence.Ready &&
+                                  ev.Elevator.CurSequence != ElevatorChamber.ElevatorSequence.Ready)) return;
+
+            var elevatorPosition = ev.Lift.Position;
             foreach (Player ply in Player.List)
             {
-                if (Vector3.Distance(ply.Position, ev.Elevator.gameObject.transform.position) < Config.Distance)
+                if (ply is null || ply.IsNPC || ply.IsHost || ply.UserId is null)
+                    continue;
+                if (Vector3.Distance(ply.Position, elevatorPosition) < Config.Distance)
                 {
-                    ply.ShowHint(Translation.ElevatorMessage.Replace("%NAME%", ev.Player.DisplayNickname), Config.HintDuration);
+                    ply.ShowHint(Translation.ElevatorMessage.Replace("%NAME%", ev.Player.DisplayNickname),
+                        Config.HintDuration);
                 }
             }
-
         }
-    }    
+    }
 }
